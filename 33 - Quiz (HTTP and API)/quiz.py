8a837f15bcc41050c3a15d1ee527b97e98cefc7e
flillo <flillo@amazon.com>
2020-03-29 15:57:32 +0200

    Final version

--- 33 - Quiz (HTTP and API)/quiz.py
+++ 33 - Quiz (HTTP and API)/quiz.py
@@ -18,16 +18,28 @@ def print_answers(opt_dictionary):
 		print(index+1, "-", opt_dictionary[index+1])
 		index += 1
 
+def incr_challenge(score):
 
-print("Welcome to the QUIZ game")
-input("Press enter to start")
+	if score == 3:
+
+		return 'medium'
+
+	elif score == 7:
+
+		return 'hard'
+
+
+print("==================== Welcome to the QUIZ game ====================")
+print("= You will be challenged with questions of increasing difficulty =")
+input("+++ Press enter to start +++")
 
 termination = False
 score = 0
+difficulty = 'easy'
 
 while(termination == False):
 
-	r = requests.get("https://opentdb.com/api.php?amount=1&category=11&difficulty=easy&type=multiple")
+	r = requests.get("https://opentdb.com/api.php?amount=1&difficulty="+difficulty+"&type=multiple")
 	if(r.status_code != 200):
 
 		print("There was an error with the connection to the database. Please restart the program.")
@@ -43,13 +55,56 @@ while(termination == False):
 		random.shuffle(options)
 		indices = [1,2,3,4]
 
-		print(question['question'])
+		print(question['question'].replace('&quot;','"').replace('&#039;',"'"))
 
 		opt_dictionary = {indices[i]: options[i] for i in range(len(options))}
 
 		print_answers(opt_dictionary)
 
-		termination = True
+		data_valid = False
 
+		while(data_valid == False):
+			try:
+				
+				user_answer = int(input("Make your selection by selecting the answer number: "))
+				data_valid = True
 
+			except:
 
+				print("ERROR: Please provide a number between 1 and 4")
+
+		if opt_dictionary[user_answer] == question['correct_answer']:
+
+			score += 1
+			print("Congratulation, correct!")
+
+		else:
+		
+			print("Nope! The right answer was:", question['correct_answer'])	
+
+		print("Your current score is:", score)	
+
+		data_valid = False
+
+		while(data_valid == False):
+
+			cont = str.lower(input("Do you want to continue? [y/n]: "))
+
+			if cont == 'y':
+
+				termination = False
+				data_valid = True
+
+				if (difficulty == 'easy' and score == 3) or (difficulty == 'medium' and score == 7):
+					print("SYSTEM: Let's bring the challenge to the next level!") 
+					difficulty = incr_challenge(score)
+
+			elif cont == 'n':
+
+				termination = True
+				data_valid = True
+				print('Your final score is', score, '. Thank you for playing!')
+
+			else:
+
+				print('Only y or n are allowed answers.')
